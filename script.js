97891a2ec45de164368eaad6b8f2526077bee098
Shubham Upadhyay <iamshubhamupadhyaya@gmail.com>
2023-04-21 21:08:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+ function changeColor(){
+	 // Get the block id and color
+	 var blockId = document.getElementById("block_id").value;
+	 var color = document.getElementById("colour_id").value;
+	 // Set the background color of the specified block
+	 document.getElementById(blockId).style.backgroundColor = color;
+}
+function resetColors(){
+	// Set the background color of all blocks to transparent
+	var blocks = document.getElementsByClassName("grid-item");
+	for(var i = 0; i < blocks.length; i++){
+		blocks[i].style.backgroundColor = "transparent";
+	}
+}
